8ada1d455c8e1a045454d9cfd84ae3bcf9131a0b
Risyandi <Risyandi.94@gmail.com>
2022-01-12 17:15:49 +0700

    fix: update todoItem

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
+import {Container, Box, Button} from 'react-bulma-components';
 
 const TodoItem = ({item, index, deleteTodoItem, completeTodoItem, updateTodoItem}) => {
     return (
-        <div className='todo-list'>
-            <li style={{textDecoration: item.status !== 0 ? "line-through" : ""}}>
-                {item.title}
-                {item.description}
-            </li>
-            <div className="btns">
-            <button onClick={() => completeTodoItem(index)}>Complete</button>
-            <button onClick={() => updateTodoItem(index)}>Update</button>
-            <button onClick={() => deleteTodoItem(index)}>X</button>
-            </div>
-        </div>
+        <Container fullhd breakpoint={'fluid'} style={{marginTop: '10px'}}>
+            <Box style={{margin: 'auto'}}>
+                <div style={{textDecoration: item.status !== 0 ? "line-through" : ""}}>
+                    <p className='item-title'>{item.title}</p>
+                    <p className='item-desc'>{item.description}</p>
+                    <p className='item-create-date'>{item.createdAt}</p>
+                </div>
+                <Button.Group>
+                    {
+                        item.status !== 1 ?
+                        <Button rounded color="success" onClick={() => completeTodoItem(index)}>Done</Button>
+                        : 
+                        <Button rounded color="success" onClick={() => completeTodoItem(index)}>Undone</Button>
+                    }
+                    <Button rounded color="primary" onClick={() => updateTodoItem(index)}>Update</Button>
+                    <Button rounded color="danger" onClick={() => deleteTodoItem(index)}>Delete</Button>
+                </Button.Group>
+            </Box>
+        </Container>
     )
 }
 
