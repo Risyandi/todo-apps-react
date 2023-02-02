cde757d519e1feee90d332aa25de563753b596b3
Risyandi <risyandi.94@gmail.com>
2023-02-02 11:10:45 +0700

    feat: update and adding modal if empty input

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -1,28 +1,36 @@
 import React, {useState} from 'react';
 import 'bulma/css/bulma.min.css';
 import {Container, Box, Form, Button, Heading} from 'react-bulma-components';
+import {ModalMessage} from './../components';
 
 const TodoInput = ({createTodoItem}) => {
     const [valueTitle, setValueTitle] = useState('');
     const [valueDesc, setValueDesc] = useState('');
+    const [showModal, setShowModal] = useState(false);
     
-    const handleSubmit = (event) => {
+    const submitTodo = (event) => {
         event.preventDefault();
-        if (valueTitle && valueDesc === "") {
-            return console.log("Please add something to-do")
+        if (valueTitle !== "" && valueDesc !== "") {
+            createTodoItem(valueTitle, valueDesc);
+            setValueTitle("");
+            setValueDesc("");
+        } else {
+            setShowModal(true);
         }
-        createTodoItem(valueTitle, valueDesc);
-        setValueTitle("");
-        setValueDesc("");
+    }
+
+    const closeModal = (status) => {
+        setShowModal(status);
     }
 
     return (
     <Container fullhd breakpoint={'fluid'} style={{marginTop: '10px'}}>
+        <ModalMessage showStatus={showModal} closeModal={closeModal}/>
         <Box style={{margin: 'auto'}}>
             <Heading subtitle>
                 Todo Application
             </Heading>
-            <Form.Control onSubmit={handleSubmit}>
+            <Form.Control onSubmit={submitTodo}>
                 <Form.Field >
                     <Form.Control>
                         <Form.Input 
@@ -46,7 +54,7 @@ const TodoInput = ({createTodoItem}) => {
                 </Form.Field>
 
                 <Button.Group>
-                    <Button fullwidth rounded color="primary" onClick={handleSubmit}>Create</Button>
+                    <Button fullwidth rounded color="primary" onClick={submitTodo}>Create</Button>
                 </Button.Group>
             </Form.Control>
         </Box>
