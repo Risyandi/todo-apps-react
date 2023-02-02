1f8cadd1e8e4b9c8e7961b8b2df15c221e632226
Risyandi <risyandi.94@gmail.com>
2023-02-02 11:11:19 +0700

    fix: update index

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// define component to export
+export { default as TodoInput } from './todoInput';
+export { default as TodoItem } from './todoItem';
+export { default as ModalMessage } from './modalMessage';
