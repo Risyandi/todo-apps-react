137be1ede70ed66e3296ec1f428ff41586bdab25
Risyandi <risyandi.94@gmail.com>
2023-02-02 11:10:18 +0700

    fix: updated and bugs fixing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, {useState, useEffect} from 'react';
-import './App.css';
 import 'bulma/css/bulma.min.css';
-import { Container } from 'react-bulma-components';
-import TodoInput from "./components/todoInput";
-import TodoItem from "./components/todoItem";
-
+import {Container} from 'react-bulma-components';
+import {TodoInput, TodoItem} from "./components";
 
 function App() {
-
-  const initTodoMajoo = []
+  const [todoItems, setTodoItems] = useState([]);
 
   useEffect(() => {
     fetchDataApi();
     // eslint-disable-next-line
   }, []);
-
-  const [todoItems, setTodoItems] = useState(initTodoMajoo);
 
   const fetchDataApi = () => {
     fetch('https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list')
@@ -24,8 +18,7 @@ function App() {
         setTodoItems(result);
       })
       .catch(error => {
-        console.log(error);
-        setTodoItems(initTodoMajoo);
+        setTodoItems([]);
       });
   };
 
@@ -47,7 +40,6 @@ function App() {
   }
 
   const completeTodoItem = (index) => {
-    console.log(index, "index");
     const newTodoItems = [...todoItems];
     newTodoItems[index].status === 0 ?
       (newTodoItems[index].status = 1) :
